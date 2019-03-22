fbd7faae782908945c49a4e52daffa80677591dc
Lance Owen Merrill <lanceomerrill@gmail.com>
2019-03-22 16:17:30 -0700

    can seed data

--- test/models/notesModel.test.js
+++ test/models/notesModel.test.js
@@ -1,16 +1,38 @@
 const Note = require('../../lib/models/Note');
+const User = require('../../lib/models/User');
+
 
 
 describe('validates note model', () => {
   it('note model', () => {
-    const note = new Note({
-      body: 'hello',
+    const user = new User({
+      name: 'lance',
       phoneNumber: '18082686581'
+    });
+    const note = new Note({
+      userId: user._id,
+      body: 'hello',
+      time: Date.now(),
+      isRepeated: false,
+      repeat: {
+        daily: true,
+        weekly: false
+      },
+      lastSent: {
+        type: Date.now()
+      }
     });
     expect(note.toJSON()).toEqual({
       _id: expect.any(Object),
+      userId: user._id,
       body: 'hello',
-      phoneNumber: '18082686581'
+      time: expect.any(Date),
+      isRepeated: false,
+      repeat: {
+        daily: true,
+        weekly: false
+      },
+      lastSent: null
     });
   });
 });
